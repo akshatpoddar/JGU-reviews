5966afc87d0b67c75cec0c9e90bd675cb609196c
akshat <akshatpoddar312002@gmail.com>
2025-04-17 14:15:46 +0530

    Fixed typo

--- jgu_course_reviewer/entrypoint.prod.sh
+++ jgu_course_reviewer/entrypoint.prod.sh
@@ -2,4 +2,4 @@
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
-python3 -m gunicorn --bind 0.0.0.0:8080 --workers 3 jgu_course_reviewier.wsgi:app
+python3 -m gunicorn --bind 0.0.0.0:8080 --workers 3 jgu_course_reviewer.wsgi:app
