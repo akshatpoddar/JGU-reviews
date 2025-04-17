4c45784515ef096821078ff22048175e11500cff
akshat <akshatpoddar312002@gmail.com>
2025-04-17 13:21:44 +0530

    A lot of dockerization changes

---
+++ jgu_course_reviewer/entrypoint.prod.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 -m gunicorn --bind 0.0.0.0:8080 --workers 3 jgu_course_reviewier.wsgi:app
